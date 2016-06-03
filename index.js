af27fd5f97a993c4d6687b85f23f899761f49cbc
Kristo Mikkonen <mikkonek@gmail.com>
2016-06-03 15:25:28 +0100

    Refresh token method and bug fixes.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class SimpleOrder {
       throw new Error('client_id, client_secret, grant_type and refresh_token are all required');
     }
 
-    this.client_id = this.client_id;
+    this.client_id = client_id;
     this.client_secret = client_secret;
     this.grant_type = grant_type;
     this.refresh_token = refresh_token;
@@ -45,7 +45,6 @@ class SimpleOrder {
       }
     };
 
-
     return new Promise((resolve, reject) => {
       request(args, function(error, response, body) {
         if (error) return reject(error);
@@ -64,6 +63,48 @@ class SimpleOrder {
       });
 
     });
+  }
+
+  refreshToken() {
+
+    var args = {
+      method: 'POST',
+      url: 'https://api.simpleorder.com/oauth2/token',
+      headers: {
+        'postman-token': 'e6e4d37e-5424-8cb2-728f-e4152099d912',
+        'cache-control': 'no-cache',
+        'content-type': 'multipart/form-data; boundary=---011000010111000001101001'
+      },
+      formData: {
+        client_id: this.client_id,
+        client_secret: this.client_secret,
+        grant_type: 'refresh_token',
+        refresh_token: this.refresh_token
+      }
+    };
+
+    return new Promise((resolve, reject) => {
+      request(args, function(error, response, body) {
+
+        //console.log(response);
+        //process.exit(0);
+        if (error) return reject(error);
+        if (Buffer.isBuffer(body)) {
+          reject(
+            new Error('something went wrong with the API call \n Request method: POST \n URL: ' + args.url + '\n data: \n ' + body.toString())
+          );
+        }
+        if ((response.statusCode) == '403') {
+          reject(
+            new Error('403: Refresh token already used.')
+          );
+        }
+        resolve(body);
+
+      });
+
+    });
+
   }
 
 }
